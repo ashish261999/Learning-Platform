683d0c9505d79c2a610ac58379b7b4add2c6b548
ashish ranjan <ashishranjanmahi07@gmail.com>
2025-04-21 22:29:00 +0530

    Created signup and login frontend and used usestate and event listner to store input form the user.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Login from "./pages/login";
 
 function App() {
 
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline m-30">Hello world!</h1>
+      <Login/>
     </>
   );
 }
---
+++ client/src/pages/login.jsx
@@ -0,0 +1,154 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+// import { sign } from "crypto";
+import { useState } from "react";
+
+const Login = () => {
+
+  // working with useState for storing details send by the user....
+
+  const [loginInput, setLoginInput] = useState({ email: "", password: "" });
+  const [signupInput, setSignupInput] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  // Input handler function to handle input send by the user.
+
+  const changeInputHandler=(e, type)=>{
+    const {name , value}= e.target;
+
+    if(type === "signup"){
+      setSignupInput({...signupInput , [name]: value});
+    }else{
+      setLoginInput({...loginInput, [name]:value});
+    }
+  }
+
+  // To get data 
+
+  const handleRegistration= (type)=>{
+
+    const inputData= type === "signup" ? signupInput: loginInput;
+    console.log(inputData);
+    // console.log(loginInput);
+  }
+
+
+  return (
+    <div className="flex items-center w-full justify-center">
+      <Tabs defaultValue="signup" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Signup</CardTitle>
+              <CardDescription>Create your Account...</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input  
+                type="text"
+                name="name"
+                value={signupInput.name}
+                placeholder="Eg. Ranjan" 
+                required="true" 
+                onChange={(e)=>{
+                  changeInputHandler(e, "signup")
+                }}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={signupInput.email}
+                  placeholder="Eg. Ranjan@googl.com"
+                  required="true"
+                  onChange={(e)=>{
+                    changeInputHandler(e, "signup")
+                  }}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Password</Label>
+                <Input
+                type="password"
+                name="password"
+                value={signupInput.password}
+                placeholder="Eg. xyz"
+                required="true"
+                onChange={(e)=>{
+                  changeInputHandler(e, "signup")
+                }}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={()=>{handleRegistration("signup")}}>Signup</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>login</CardTitle>
+              <CardDescription>
+                Loign using Email and Password...
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="current">Email</Label>
+                <Input
+                  type="email"
+                  name="email"
+                  value={loginInput.email}
+                  placeholder="Eg. Ranjan@googl.com"
+                  required="true"
+                  onChange={(e)=>{
+                    changeInputHandler(e, "login")
+                  }}
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">Password</Label>
+                <Input 
+                type="password" 
+                name ="password"
+                value={loginInput.password}
+                placeholder="Eg. xyz" 
+                required="true" 
+                onChange={(e)=>{
+                  changeInputHandler(e, "login")
+                }}
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button  onClick={()=>{handleRegistration("signup")}} >Login</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Login;
